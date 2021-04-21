ee65721583d61aad5adbba23f2dfadbbf7a355a6
motoshifurugen <amanogawa_m@icloud.com>
2021-04-21 22:54:15 +0900

    削除機能追加

--- src/views/AdminNews.vue
+++ src/views/AdminNews.vue
@@ -1,64 +1,232 @@
 <template>
-    <div class="news">
-        <v-card v-for="news in info.data" :key="news.id">
+  <v-data-table
+    :headers="headers"
+    :items="info.data"
+    sort-by="id"
+    class="elevation-1"
+  >
+    <template v-slot:top>
+      <v-toolbar
+        flat
+      >
+        <v-toolbar-title>お知らせ一覧</v-toolbar-title>
+        <v-divider
+          class="mx-4"
+          inset
+          vertical
+        ></v-divider>
+        <v-spacer></v-spacer>
+        <v-dialog
+          v-model="dialog"
+          max-width="500px"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              color="primary"
+              dark
+              class="mb-2"
+              v-bind="attrs"
+              v-on="on"
+            >
+              新規作成
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="headline">{{ formTitle }}</span>
+            </v-card-title>
+
             <v-card-text>
-                <div>{{ news["created_at"] | moment }}</div>
-                <p class="display-1 text--primary">
-                    {{ news["title"] }}
-                </p>
-                <v-list-item>
-                <v-list-item-content>
-                <div class="text--primary">
-                    {{ news["body"] }}
-                </div>
-                </v-list-item-content>
-                </v-list-item>
+              <v-container>
+                <v-row>
+                  <v-col
+                    cols="12"
+                    sm="12"
+                    md="12"
+                  >
+                    <v-text-field
+                      v-model="editedItem.title"
+                      label="タイトル"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="12"
+                    md="12"
+                  >
+                    <v-text-field
+                      v-model="editedItem.body"
+                      label="内容"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
             </v-card-text>
-        </v-card>
-        <v-btn to="/admin">管理ページトップ</v-btn>
-    </div>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="blue darken-1"
+                text
+                @click="close"
+              >
+                キャンセル
+              </v-btn>
+              <v-btn
+                color="blue darken-1"
+                text
+                @click="save"
+              >
+                保存する
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="headline">このお知らせを削除します。よろしいですか？</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeDelete">キャンセル</v-btn>
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template v-slot:item.actions="{ item }">
+      <v-icon
+        small
+        class="mr-2"
+        @click="editItem(item)"
+      >
+        mdi-pencil
+      </v-icon>
+      <v-icon
+        small
+        @click="deleteItem(item)"
+      >
+        mdi-delete
+      </v-icon>
+    </template>
+    <template v-slot:no-data>
+      <v-btn
+        color="primary"
+        @click="initialize"
+      >
+        リセット
+      </v-btn>
+    </template>
+  </v-data-table>
 </template>
 
-<style scoped>
-.news {
-    width: 100%;
-}
-.news p {
-    font-size: 1.5em !important;
-}
-.v-card--reveal {
-    bottom: 0;
-    opacity: 1 !important;
-    position: absolute;
-    width: 100%;
-}
-</style>
-
 <script>
-import axios from 'axios'
+import axios from 'axios';
 import moment from 'moment';
 
-export default {
-    data(){
-        return{
-            reveal: false,
-            info: null,
-        }
-    },
-    computed: {
-        reverseNewsLetters() {
-            return this.news_letters.slice().reverse();
+  export default {
+    data: () => ({
+      dialog: false,
+      dialogDelete: false,
+      headers: [
+        { text: 'ID', value: 'id' },
+        {
+          text: 'タイトル',
+          align: 'start',
+          sortable: false,
+          value: 'title',
         },
+        { text: '内容', sortable: false, value: 'body' },
+        { text: '作成日', value: 'created_at' },
+        { text: '操作', value: 'actions', sortable: false },
+      ],
+      info: [],
+      editedIndex: -1,
+      editedItem: {
+        title: '',
+        body: '',
+      },
+      defaultItem: {
+        title: '',
+        body: '',
+      },
+    }),
+
+    computed: {
+      formTitle () {
+        return this.editedIndex === -1 ? '新規作成' : '更新'
+      },
     },
-    mounted () {
-        axios
-            .get('http://cocoahearts.xsrv.jp/api/letters')
-            .then(response => (this.info = response))
-    },
+
     filters: {
         moment(date) {
             return moment(date).format('YYYY/MM/DD');
         },
-    }
-};
+    },
+
+    watch: {
+      dialog (val) {
+        val || this.close()
+      },
+      dialogDelete (val) {
+        val || this.closeDelete()
+      },
+    },
+
+    created () {
+      this.initialize()
+    },
+
+    methods: {
+      initialize () {
+        axios
+            .get('http://cocoahearts.xsrv.jp/api/letters')
+            .then(response => (this.info = response))
+      },
+
+      editItem (item) {
+        this.editedIndex = this.desserts.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+      },
+
+      deleteItem (item) {
+        this.editedIndex = item.id
+        this.editedItem = Object.assign({}, item)
+        this.dialogDelete = true
+      },
+
+      deleteItemConfirm () {
+        axios.delete('http://cocoahearts.xsrv.jp/api/letters/'+this.editedIndex).then(() => 
+            this.$router.go({path: this.$router.currentRoute.path, force: true}));
+            this.closeDelete()
+      },
+
+      close () {
+        this.dialog = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+      },
+
+      closeDelete () {
+        this.dialogDelete = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+      },
+
+      save () {
+        if (this.editedIndex > -1) {
+          Object.assign(this.desserts[this.editedIndex], this.editedItem)
+        } else {
+          this.desserts.push(this.editedItem)
+        }
+        this.close()
+      },
+    },
+  }
 </script>
